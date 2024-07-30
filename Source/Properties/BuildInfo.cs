238e1ff040f2a3316e5755c5a554d1e2cf6534c5
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-07-30 16:22:06 +0930

    Spanish and German translations :)

--- Source/Properties/BuildInfo.cs
+++ Source/Properties/BuildInfo.cs
@@ -2,23 +2,22 @@
 
 internal static class BuildInfo
 {
-    #region Main
+    // Main
     public const string Name = "ItemRarities";
     public const string Description = null!;
     public const string Author = "Deadman";
     public const string Copyright = "Copyright © 2023 - 2024";
-    public const string Version = "2.0.1";
-    public const string AssemblyVersion = $"{Version}.0";
+    public const string Version = "2.0.2-DeveloperBuild";
+    public const string AssemblyVersion = $"2.0.2.0";
+    //public const string AssemblyVersion = $"{Version}.0";
     public const string MelonLoaderVersion = "0.6.1";
     public const string GUIName = "Item Rarities";
-    #endregion
 
-    #region Optional
+    // Optional
     public const string Company = null!;
     public const string DownloadLink = null!;
     public const string Trademark = null!;
     public const string Product = Name;
     public const string Culture = null!;
     public const int Priority = 0;
-    #endregion
 }
